ec95a5084480cf15515895a344459a018c8e572c
dasWarder <Al.94_94@mail.ru>
2021-10-11 15:19:46 +0300

    Add Eureka client dependenci and make this microservice be able to register on eurika server

--- src/main/java/com/example/microservices_userservice/MicroservicesUserServiceApplication.java
+++ src/main/java/com/example/microservices_userservice/MicroservicesUserServiceApplication.java
@@ -2,7 +2,9 @@ package com.example.microservices_userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicesUserServiceApplication {
 
